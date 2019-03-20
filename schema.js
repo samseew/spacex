c39e926ff398bc54ca68424317a9996e7cde1266
samashtar <sam.ashtar@flatironschool.com>
2019-03-19 23:33:46 -0400

    schema graphql

--- schema.js
+++ schema.js
@@ -1,12 +1,51 @@
 //all the graphql stuff here
 
-
-const GraphQLObjectType = require('graphql')
+//so every set of data you wanna grab is essentially an object type 
+const {
+    GraphQLObjectType,
+    GraphQLInt,
+    GraphQLString,
+    GraphQLBoolean
+} = require('graphql')
 
 // Launch Type
 const LaunchType = new GraphQLObjectType({
     name: 'Launch',
-    fields: () => ({
+    fields: () => ({ // these are the fields you are grabbing from the giant API
+        flight_number: {
+            type: GraphQLInt
+        },
+        mission_name: {
+            type: GraphQLString
+        },
+        launch_year: {
+            type: GraphQLString
+        },
+        launch_date_local: {
+            type: GraphQLString
+        },
+        launch_sucess: {
+            type: GraphQLBoolean
+        },
+        rocket: { // RocketType is a whole separate object type, this way we create a nested object type
+            type: RocketType
+        }
+    })
+})
 
+
+//Rocket Type
+const RocketType = new GraphQLObjectType({
+    name: 'Rocket',
+    fields: () => ({ // these are the fields you are grabbing from the giant API
+        rocket_id: {
+            type: GraphQLString
+        },
+        rocket_name: {
+            type: GraphQLString
+        },
+        rocket_type: {
+            type: GraphQLString
+        }
     })
 })
