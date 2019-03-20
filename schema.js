a442c8e12a2cac085c5dc2735986936a54b05daa
samashtar <sam.ashtar@flatironschool.com>
2019-03-20 00:06:46 -0400

    graphql set up

--- schema.js
+++ schema.js
@@ -28,7 +28,7 @@ const LaunchType = new GraphQLObjectType({
         launch_date_local: {
             type: GraphQLString
         },
-        launch_sucess: {
+        launch_success: {
             type: GraphQLBoolean
         },
         rocket: { // RocketType is a whole separate object type, this way we create a nested object type
@@ -57,12 +57,45 @@ const RocketType = new GraphQLObjectType({
 //Root Query - this is actually using the nested object type we created and getting it 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
-    fields: {
+    fields: { //these are your restful equivalents
+        // all launches
         launches: {
             type: new GraphQLList(LaunchType),
             resolve(parent, args) {
                 // return axios.get('https://api.spacexdata.com/v3/launches').then(res => res.data)
                 return fetch('https://api.spacexdata.com/v3/launches').then(res => res.json())
+            }
+        },
+        //single launch
+        launch: {
+            type: LaunchType,
+            args: {
+                flight_number: {
+                    type: GraphQLInt
+                }
+            },
+            resolve(parent, args) {
+                return fetch(`https://api.spacexdata.com/v3/launches/${args.flight_number}`).then(res => res.json())
+            }
+        },
+        // All rockets
+        rockets: {
+            type: new GraphQLList(RocketType),
+            resolve(parent, args) {
+                // return axios.get('https://api.spacexdata.com/v3/launches').then(res => res.data)
+                return fetch('https://api.spacexdata.com/v3/rockets').then(res => res.json())
+            }
+        },
+        //single rocket
+        rocket: {
+            type: RocketType,
+            args: {
+                id: {
+                    type: GraphQLInt
+                }
+            },
+            resolve(parent, args) {
+                return fetch(`https://api.spacexdata.com/v3/rockets/${args.id}`).then(res => res.json())
             }
         }
     }
