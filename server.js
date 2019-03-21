70c1f5309aff48b76f3e8303d35224a2cda06522
samashtar <sam.ashtar@flatironschool.com>
2019-03-20 23:17:39 -0400

    cors fixed

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-const express = require('express')
-const graphqlHTTP = require('express-graphql')
-const schema = require('./schema')
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema");
+const cors = require("cors");
 
-const app = express()
+const app = express();
+
+//allow cross-origin
+app.use(cors());
 
 //with graphql, you only use /graphql as one endpoint route for the entire app
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema: schema,
     graphiql: true
-}))
+  })
+);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
