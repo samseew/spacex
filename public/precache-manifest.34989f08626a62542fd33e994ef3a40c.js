e67364f6bd87db943ea01cd03ee357c000fc2821
samashtar <sam.ashtar@flatironschool.com>
2019-03-21 00:18:22 -0400

    deploy to heroku

---
+++ public/precache-manifest.34989f08626a62542fd33e994ef3a40c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb9bd6d40006c85aa967",
+    "url": "/static/css/main.253b301d.chunk.css"
+  },
+  {
+    "revision": "eb9bd6d40006c85aa967",
+    "url": "/static/js/main.d13261aa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f85511349f204c800342",
+    "url": "/static/js/2.c7f68b73.chunk.js"
+  },
+  {
+    "revision": "c8458b3ea407cdb0b2ca38b968cbe0d3",
+    "url": "/static/media/logo.c8458b3e.jpg"
+  },
+  {
+    "revision": "84a3e01a9f346d3525cb93e3dd4b341c",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -16,6 +17,12 @@ app.use(
     graphiql: true
   })
 );
+
+app.use(express.static("public"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
